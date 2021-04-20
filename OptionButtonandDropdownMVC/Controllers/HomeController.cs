1653e619ba060ebe5470554f3d6b58b7e06b8787
Elizabethpv <48952600+Elizabethpv@users.noreply.github.com>
2021-04-20 11:15:51 +0530

    Add project files.

---
+++ OptionButtonandDropdownMVC/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using OptionButtonandDropdownMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OptionButtonandDropdownMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult StudentView(Student student)
+        {
+            return View("About",student);
+        }
+
+    }
+}
---
+++ OptionButtonandDropdownMVC/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OptionButtonandDropdownMVC.Models
+{
+    public class Student
+    {
+        public String Name { get; set; }
+        public String Gender { get; set; }
+        public String Education { get; set; }
+        public int Value { get; set; }
+        public String Text { get; set; }
+
+        public String Status { get; set; }
+
+    }
+}
